6ebcff0d32e936683908739bf673a1e9f4858ec8
benhost <quan.luu@ntq-solution.com.vn>
2022-08-26 08:13:40 +0700

    simple for power plant

--- src/test/java/com/ben/SampleApplicationTests.java
+++ src/test/java/com/ben/SampleApplicationTests.java
@@ -1,45 +1,76 @@
 package com.ben;
 
-import com.ben.controller.StudentController;
-import com.ben.service.StudentService;
+import com.ben.dto.request.BatteryRequestDto;
+import com.ben.service.BatteryService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertEquals;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.Mockito.mock;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ActiveProfiles("test")
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {SampleApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@SpringBootTest
 public class SampleApplicationTests {
 
-    private final Logger log = LoggerFactory.getLogger(this.getClass());
+    ObjectMapper objectMapper = new ObjectMapper();
+    private BatteryService service;
 
-	private  StudentService service;
-	private StudentController victim;
+    @Autowired
+    private MockMvc mockMvc;
 
-	@Before
-	public void setup() {
-		service = mock(StudentService.class);
-		victim = new StudentController(service);
-	}
-
-    @Test
-    public void contextLoads() {
-        log.info("test");
-        assertThat(true);
+    @Before
+    public void setup() {
+        service = mock(BatteryService.class);
     }
 
     @Test
-    public void testTriggerException() throws Exception {
-        assertEquals(victim.getAll().size(), 0);
+    public void it_should_return_create_battery() throws Exception {
+        List<BatteryRequestDto> batteryDtos = createBatteryDtoInput();
+        service.savingData(batteryDtos);
+        mockMvc.perform(post("/api/v1/batteries")
+                .content(objectMapper.writeValueAsString(batteryDtos))
+                .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isCreated());
+    }
+
+    @Test
+    public void retrieved_battery_and_validate_total_and_average_capacity() throws Exception {
+        long postCodeFrom = 0L;
+        long postCodeTo = 6L;
+
+        List<BatteryRequestDto> batteryDtos = createBatteryDtoInput();
+
+        MockHttpServletRequestBuilder requestBuilder = get("/api/v1/batteries");
+        requestBuilder.param("postcodeFrom", String.valueOf(postCodeFrom));
+        requestBuilder.param("postcodeTo", String.valueOf(postCodeTo));
+        mockMvc.perform(requestBuilder.contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("batteryDtos[0]").value(batteryDtos.get(0)))
+                .andExpect(jsonPath("totalCapacity").value(9))
+                .andExpect(jsonPath("averageCapacity").value(4.5));
+    }
+
+    private List<BatteryRequestDto> createBatteryDtoInput() {
+        List<BatteryRequestDto> batteries = new ArrayList<>();
+        batteries.add(new BatteryRequestDto().setName("Bat1").setPostcode(1L).setCapacity(3.3));
+        batteries.add(new BatteryRequestDto().setName("New Bat").setPostcode(5L).setCapacity(5.7));
+        batteries.add(new BatteryRequestDto().setName("Old Bat").setPostcode(9L).setCapacity(10.7));
+        return batteries;
     }
 
 }
